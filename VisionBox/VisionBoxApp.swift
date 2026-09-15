15d2d3522f2ac99f6db2dd68a37c290b66326cab
Lk <luke.m.advincula@accenture.com>
2026-09-15 14:34:51 +0800

    Initial Commit

---
+++ VisionBox/VisionBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoxApp.swift
+//  VisionBox
+//
+//  Created by Luke Advincula on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
